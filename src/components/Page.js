7ebf0c761ffbc6b0c13757d4695740ae5c9c370a
Aholap <patrik.ahola@hotmail.com>
2021-04-16 19:24:51 +0300

    updates

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,37 +1,48 @@
+
 import React, { useState,  } from 'react';
-
-
-
+const parse = require('html-react-parser');
 
 const Page = (page) => {
 
+
+    
     const [show, setShow] = useState(false)
+    const [divButShow, setDivButShow] = useState(false)
 
+    
+    
+    const a = parse.htmlToDOM(page.page.description.body)
+    console.log(a)
+    const b = parse.domToReact(a)
+    console.log(b)
 
-    console.log('PAGE', page)
+    console.log(<div></div>)
 
+    
     if(!show){
 
         return(
         
             <button className='button' onClick = {() => setShow(show === false ? true : false)}>+ {page.page.name.fi} </button>
             
-        
-
+            
     )
 
     }
     else{
+
+
+
         return(
         <div >
             <button className='button' onClick = {() => setShow(show === false ? true : false)}>- {page.page.name.fi} </button>
-               
-               <div className="body">
+            
+            <div className="body">
                 <p >
                     {page.page.name.en}
                 </p>
                 <p >
-                    Missä ja milloin? : {page.page.where_when_duration.where_and_when} 
+                    Missä ja milloin? : {page.page.where_when_duration.where_and_when ? page.page.where_when_duration.where_and_when : "No info"} 
                 </p>
                 <p >
                     Kesto: {page.page.where_when_duration.duration ? page.page.where_when_duration.duration : "Ei tietoa"} 
@@ -42,9 +53,25 @@ const Page = (page) => {
                 <a href={page.page.info_url}>{page.page.info_url}</a>
                     
                 </p>
-                </div>
+
+            <button className="button"  onClick = {() => setDivButShow(divButShow === false ? true : false)}>
+
+                {divButShow ? "- Less Info" : "+ More Info"}
+            </button>
+                
+                {divButShow ? b : null}
+
+            </div>
+
+           
+                
         </div>
+
+
+
         )
+        
+        
     }
     
 }
