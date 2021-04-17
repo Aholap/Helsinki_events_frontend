1e24208adf73f58b97f4a5984551d7d6856c1f19
Aholap <patrik.ahola@hotmail.com>
2021-04-17 19:13:41 +0300

    scrolltotopbutton

--- src/App.js
+++ src/App.js
@@ -17,6 +17,41 @@ const App =() => {
   
   const api_key = process.env.KEY
   const [siteData, setData] = useState(null)
+
+  var topButton = null
+  useEffect(() => {
+    topButton = document.getElementById("toTop")
+    
+  })
+
+
+  const scrollToPageTop = () => {
+    
+    document.body.scrollTop = 550
+    document.documentElement.scrollTop = 550
+  }
+
+  
+
+  const showScrollButton = () => {
+    console.log(topButton)
+
+    if (topButton) {
+
+      if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000){
+        topButton.style.display = "block"
+      }
+      else{
+        
+        topButton.style.display = "none"
+      }
+    }
+
+
+  }
+
+  window.onscroll = showScrollButton
+
   
 
   const get_api_data =async () => {
@@ -56,11 +91,19 @@ const App =() => {
 
     return(
       <div className='sub-bod'>
+
+        <button id="toTop" className="topButton" onClick = {() => scrollToPageTop()}  >
+          Scroll to top
+        </button>
+
+
         <h1 className='App-header'>
         Tapahtumat Helsingissä
         </h1>
 
-      
+
+
+
         <LoadScript
         
         id="script-loader"
@@ -71,10 +114,10 @@ const App =() => {
         
         id="circle-example"
         mapContainerStyle={{
-          height: 400,
-          width: 600,
+          height : 400,
+          width : window.width,
           position:'relative',
-          left:340
+          
           
         }}
         zoom={9}
@@ -101,7 +144,12 @@ const App =() => {
         {
         siteData.map(d => 
         <Page key={d.id} page={d}/>)}
+
+
+
      
+        
+
 
 
 
